a2154921307fc1d210f1219e7282354691fc443c
Robert Stuttaford <robert@cognician.com>
2014-03-21 10:23:12 +0200

    Upgrade ClojureScript, Om. Add Dommy, cljx. Add cljx config.

--- project.clj
+++ project.clj
@@ -9,9 +9,9 @@
   :dependencies [[ring/ring-core "1.2.1" :exclusions [org.clojure/tools.reader]]
                  [javax.servlet/servlet-api "2.5"]
                  [org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2156"]
+                 [org.clojure/clojurescript "0.0-2173"]
                  [org.clojure/core.async "0.1.278.0-76b25b-alpha"]
-                 [om "0.5.2"]
+                 [om "0.5.3"]
                  [http-kit "2.1.17"]
                  [compojure "1.1.6"]
                  [fogus/ring-edn "0.2.0"]
@@ -20,21 +20,36 @@
                  [datomic-schema "1.0.2"]
                  [org.clojure/tools.nrepl "0.2.3" :exclusions [org.clojure/clojure]]
                  [ring-anti-forgery "0.3.0"]
-                 ;;[clojure-complete "0.2.3"]
+                 [prismatic/dommy "0.1.2"]
+                 [sablono "0.2.14" :exclusions [com.facebook/react]]
+                 [ankha "0.1.1"]
                  [environ "0.4.0"]]
 
-  :plugins [[lein-cljsbuild "1.0.2"]]
+  :plugins [[lein-cljsbuild "1.0.2"]
+            [com.keminglabs/cljx "0.3.2"]]
 
-  :source-paths ["src/clj" "src/cljs"]
+  :source-paths ["src/clj" "target/generated/clj"]
   :resource-paths ["resources"]
 
   :cljsbuild
   {:builds
    [{:id "dev"
-     :source-paths ["src/clj" "src/cljs"]
+     :source-paths ["src/clj" "src/cljs" "target/generated/cljs"]
      :compiler
      {:output-to "resources/public/js/main.js"
       :output-dir "resources/public/js/out"
+      :preamble ["react/react.js"]
+      :externs ["react/externs/react.js"]
       :optimizations :none
       :source-map true}}]}
+
+  :cljx
+  {:builds
+   [{:source-paths ["src/cljx/"]
+     :output-path "target/generated/clj"
+     :rules :clj}
+    {:source-paths ["src/cljx/"]
+     :output-path "target/generated/cljs"
+     :rules :cljs}]}
+
   :main sptcg.core)
