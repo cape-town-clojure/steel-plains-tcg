6e2771ac8d0d3b30b67cdf430476096af7dab158
Deon Moolman <cmdrdats@gmail.com>
2014-03-25 11:18:04 +0200

    Small tweaks to the schema - seperate a player (which is in a game) from a user (which owns collections of cards and decks)

--- src/clj/sptcg/schema.clj
+++ src/clj/sptcg/schema.clj
@@ -16,12 +16,23 @@
 
 ;; See http://docs.datomic.com/schema.html for more detailed information
 
+(def basic-land-types #{:plains :mountain :river :hills :swamp :tundra :forest :mudflats})
+(def card-types #{:land :creature :structure :effect :enchantment :equipment})
 
-(defschema player
+;; The user is a person that owns cards
+(defschema user
   (fields
    [name :string :indexed]
    [pwd :string "Hashed password string"]
    [email :string :indexed :unique-value]))
+
+;; The Player represents a player in a single given game
+(defschema player
+  (fields
+   [user :ref]
+   [hand :ref :many]
+   [land-deck :ref :many]
+   [spell-deck :ref :many]))
 
 (defschema game
   (fields
@@ -31,26 +42,35 @@
 
 (defschema owner
   (fields
+   [user :ref]
    [player :ref]
    [game :ref]))
 
-(defschema position
+;; Entities are entities in a given game.
+(defschema entity
   (fields
    [x :long]
-   [y :long]))
+   [y :long]
+   [health :long]
+   [attack :long]
+   [card :ref "The card used to create this entity, if any"]
+   [game :ref "The game this entity is in"]))
 
+;; An individual card, owned by a user (not in a game)
 (defschema card
   (fields
    [cardtype :ref]
    [foil :boolean]))
 
+;; A deck owned by a user, refers to specific cards, not carddefs
 (defschema deck
   (fields
    [name :string :indexed]
    [spells :ref :many]
    [lands :ref :many]))
 
-(defschema cardtype
+;; A definition of a card
+(defschema carddef
   (fields
    [name :string :indexed]
    [text :string]
@@ -63,6 +83,7 @@
    [deck :ref]
    [size :long]))
 
+;; A production or cost entry for a card.
 (defschema mana
   (fields
    [colour :enum (vec card-schema/colours)]
